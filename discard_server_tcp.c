ca067097af82ee73aae20aef22eefda00f605a35
todsagt <jj914917@fh-muenster.de>
2016-11-10 17:55:11 +0100

    Prak 2 abgenommen

--- discard_server_tcp.c
+++ discard_server_tcp.c
@@ -8,24 +8,24 @@
 #include <stdio.h>
 #include "Socket.h"
 
+#define MAXCLIENTS 10
+
 int main(int argc, char **argv)
 {
-    int sock, tmpsock, sockmax, i;
+    int sock, tmpsock, i;
     int on = 1;
     fd_set sock_set;
-    struct sockaddr_in server_addr, client_addr;
+    struct sockaddr_in server_addr;
     char buff[1000];
-    socklen_t len;
     ssize_t size;
-
-    FD_ZERO(&sock_set);
-
     sock = Socket(AF_INET, SOCK_STREAM, 0);
     setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (const void *) &on, sizeof(int));
+    
 
-    FD_SET(sock, &sock_set);
-    sockmax = sock;
-
+    int clientarray[MAXCLIENTS];
+    for(i=0;i < MAXCLIENTS;i++) {
+	clientarray[i]= -1;
+    }
     memset((void *) &server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -33,30 +33,46 @@ int main(int argc, char **argv)
     Bind(sock, (struct sockaddr *) &server_addr, sizeof(server_addr));
     Listen(sock, 8);
     while (1) {
-        Select(sockmax+1,&sock_set, NULL, NULL, NULL);
-        for(i=0;i <= sockmax;i++) {
-            if (FD_ISSET(i,&sock_set)) {
-                if(i == sock) { //neue verbindung
-                    printf("Neue Verbindung\n");
-                    len = sizeof(client_addr);
-                    tmpsock = Accept(sock, (struct sockaddr *) &client_addr, &len); //fd für neue Verbindung
-                    FD_SET(tmpsock,&sock_set); // neue Verbindung ins fd-array
-                    if (tmpsock > sockmax)
-                        sockmax = tmpsock; // neue sockmax
-                } else { // daten von clienten bearbeiten
-                    size = 1;
-                    while( size > 0) {                          // schmeiß weg
-                        size = Recv(i, buff, sizeof(buff), 0);  // lass liegen, tritt sich fest
-			if(size == 0)
-                        printf("Daten wegwerfen: %.*s\n",(int)size,buff);
-			else
-			printf("Daten wegwerfen: %.*s",(int)size,buff);
+	FD_ZERO(&sock_set);
+	FD_SET(sock, &sock_set);
+	int max = sock;
+	for(i=0;i < MAXCLIENTS;i++){
+	    if(clientarray[i] == -1) {
+		continue;
+	    } else {
+		if(max<clientarray[i]) {
+		    max = clientarray[i]; // max für Select bestimmen
+		}
+		FD_SET(clientarray[i],&sock_set);
+	    }
+	}
+        Select(max+1,&sock_set, NULL, NULL, NULL);
+	if(FD_ISSET(sock,&sock_set)) {
+	    tmpsock = Accept(sock, NULL, NULL);
+	    for(i=0;i < MAXCLIENTS;i++){
+	    	if(clientarray[i] == -1) {
+		    printf("Neue Verbindung\n");
+	    	    clientarray[i] = tmpsock;
+		    break;
+		}
+	    }
+	    if(i == MAXCLIENTS)
+	    Close(tmpsock);
+	}
+	for(i=0;i < MAXCLIENTS;i++) {
+	    if(clientarray[i] == -1) {
+		break;
+	    } else {
+            	if (FD_ISSET(clientarray[i],&sock_set)) { // daten von clienten bearbeiten
+                    size = Recv(clientarray[i], buff, sizeof(buff), 0);
+		    if(size == 0) {
+		    	Close(clientarray[i]);
+		    	printf("Verbindung geschlossen\n");
+		    	clientarray[i] = -1;
+		    } else {
+		    	printf("Daten wegwerfen: %.*s",(int)size,buff);
                     }
-                    Close(i);
-		    printf("Verbindung geschlossen\n");
-                    FD_CLR(i,&sock_set);
-                    sockmax = sock;
-                }
+		}
             }
         }
     }
